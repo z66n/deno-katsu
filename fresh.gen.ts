0839942fc43ddbd11fbc9c037b3d783dee84e12c
z66n <47192580+z66n@users.noreply.github.com>
2025-07-13 20:36:57 +0000

    Updated gitignore

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,7 +6,6 @@ import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $api_joke from "./routes/api/joke.ts";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
-import * as $index_bak from "./routes/index.bak.tsx";
 import * as $index from "./routes/index.tsx";
 import * as $random from "./routes/random.tsx";
 import * as $Counter from "./islands/Counter.tsx";
@@ -20,7 +19,6 @@ const manifest = {
     "./routes/_app.tsx": $_app,
     "./routes/api/joke.ts": $api_joke,
     "./routes/greet/[name].tsx": $greet_name_,
-    "./routes/index.bak.tsx": $index_bak,
     "./routes/index.tsx": $index,
     "./routes/random.tsx": $random,
   },
